59bdff77988b169bcdc88ca8e71d0edd6ce498e5
Tahsin Al Mahi <tahsinalmahi@yahoo.com>
2020-09-26 15:42:43 +0600

    Added login system

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 const express = require('express')
 const hbs = require('hbs')
 const mongoose = require('mongoose')
+const session = require('express-session')
 require('./db/mongoose')
+const passport = require('passport')
+const {authentication, secureAuthentication} = require('./authentication/auth')
 const User = require('./models/user')
 
 const app = express()
+
+app.use(session({
+    secret: 'mango',
+    resave: false,
+    saveUninitialized: false
+}))
 
 app.use(express.static('./public'))
 app.set('view engine', 'hbs')
@@ -12,9 +21,9 @@ app.set('views', './public/templates/views')
 hbs.registerPartials('./public/templates/partials')
 
 app.use(express.json())
-app.use(express.urlencoded({
-    extended: false
-}))
+app.use(express.urlencoded({extended: false}))
+app.use(passport.initialize())
+app.use(passport.session())
 
 app.get('/', function (req, res) {
     res.render('index.hbs', {
@@ -22,7 +31,7 @@ app.get('/', function (req, res) {
     })
 })
 
-app.get('/about', function (req, res) {
+app.get('/about', secureAuthentication, function (req, res) {
     res.render('about.hbs', {
         title: 'About us'
     })
@@ -42,6 +51,29 @@ app.post('/register', async function(req, res) {
     } catch (error) {
         res.redirect('back')
     }
+})
+
+app.post('/login', async function(req, res, next) {
+    try {
+        await passport.authenticate('local', {
+            successRedirect: '/profile',
+            failureRedirect: '/'
+        })(req, res, next)
+    } catch (error) {
+        // res.redirect('back')
+        console.log(error)
+    }
+})
+
+app.get('/profile', secureAuthentication, function(req, res) {
+    res.render('profile.hbs', {
+        user: req.user
+    })
+})
+
+app.get('/logout', function(req, res) {
+    req.logout()
+    res.redirect('/')
 })
 
 app.get('*', function (req, res) {
